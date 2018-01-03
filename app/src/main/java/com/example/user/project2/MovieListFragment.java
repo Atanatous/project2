59e22057e4e13ea18994bdc05b70c84e6b252785
Atanatous <chanm1996@gmail.com>
2018-01-04 06:59:39 +0900

    Finish. Maybe?

--- app/src/main/java/com/example/user/project2/MovieListFragment.java
+++ app/src/main/java/com/example/user/project2/MovieListFragment.java
@@ -90,6 +90,10 @@ public class MovieListFragment extends Fragment {
     private ArrayList<ListViewItem> mItemList = new ArrayList<>();
     private ListViewAdapter mAdapter;
     private String userId;
+    private String[] movieTitle =
+            {"The_City_of_Crime", "Dark_Knight",
+            "Good,_Bad_,Strange", "Titanic",
+            "The_Wolf_of_Wallstreet"};
 
     // Main function.
     // Make ListView and set listeners on it.
@@ -105,12 +109,13 @@ public class MovieListFragment extends Fragment {
         username.setText(userId);
         commentNum.setText("5");
 
-        mItemList.clear();
-        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.crimecity), "The City Of Crime", 3.5, "범죄들의 천국"));
-        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.darkknight), "Dark Knight", 4.2, "어두운 밤"));
-        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.nom3), "Good, Bad, Strange", 3.8, "모든 놈들은 항상 이상했다."));
-        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.titanic), "Titanic", 4.4, "백허그밖에 기억나지 않아요"));
-        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.wolf), "The Wolf of Wallstreet", 3.7, "더 늑대 구슬 벽 거리 거닐다."));
+        new JSONTaskServer().execute("http://13.125.74.215:8080/api/articles/");
+
+//        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.crimecity), "The City of Crime", 3.5, "범죄들의 천국"));
+//        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.darkknight), "Dark Knight", 4.2, "어두운 밤"));
+//        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.nom3), "Good, Bad, Strange", 3.8, "모든 놈들은 항상 이상했다."));
+//        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.titanic), "Titanic", 4.4, "백허그밖에 기억나지 않아요"));
+//        mItemList.add(new ListViewItem(ContextCompat.getDrawable(getActivity(), R.drawable.wolf), "The Wolf of Wallstreet", 3.7, "더 늑대 구슬 벽 거리 거닐다."));
 
         mAdapter = new ListViewAdapter();
         mListView = (ListView) v.findViewById(R.id.listview_movies);
@@ -145,6 +150,120 @@ public class MovieListFragment extends Fragment {
 
 
         return v;
+    }
+
+
+    public class JSONTaskServer extends AsyncTask<String, String, String> {
+
+        protected String doInBackground(String... urls) {
+            try {
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try{
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);//url을 가져온다.
+                    con = (HttpURLConnection) url.openConnection();
+                    con.connect();//연결 수행
+
+                    //입력 스트림 생성
+                    InputStream stream = con.getInputStream();
+
+                    //속도를 향상시키고 부하를 줄이기 위한 버퍼를 선언한다.
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    //실제 데이터를 받는곳
+                    StringBuffer buffer = new StringBuffer();
+
+                    //line별 스트링을 받기 위한 temp 변수
+                    String line = "";
+
+                    //아래라인은 실제 reader에서 데이터를 가져오는 부분이다. 즉 node.js서버로부터 데이터를 가져온다.
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    //다 가져오면 String 형변환을 수행한다. 이유는 protected String doInBackground(String... urls) 니까
+                    return buffer.toString();
+
+                    //아래는 예외처리 부분이다.
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    //종료가 되면 disconnect메소드를 호출한다.
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        //버퍼를 닫아준다.
+                        if(reader != null){
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }//finally 부분
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        //doInBackground메소드가 끝나면 여기로 와서 리스트뷰의 값을 바꿔준다.
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                Drawable wolf = ContextCompat.getDrawable(getActivity(), R.drawable.wolf);
+                Drawable crimecity = ContextCompat.getDrawable(getActivity(), R.drawable.crimecity);
+                Drawable titanic = ContextCompat.getDrawable(getActivity(), R.drawable.titanic);
+                Drawable nom3 = ContextCompat.getDrawable(getActivity(), R.drawable.nom3);
+                Drawable darknight = ContextCompat.getDrawable(getActivity(), R.drawable.darkknight);
+
+                for (int i=0; i<jsonArray.length(); i++)
+                {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i); //i번째 Json데이터를 가져옴
+                    String title = jsonObject.getString("title");
+                    Double score = jsonObject.getDouble("score");
+                    String description = jsonObject.getString("description");
+                    ListViewItem listViewItem;
+                    title = title.replace("_", " ");
+                    switch (title) {
+                        case "The City of Crime":
+                            listViewItem = new ListViewItem(crimecity, title, score, description);
+                            mItemList.add(listViewItem);
+                            break;
+                        case "Dark Knight":
+                            listViewItem = new ListViewItem(darknight, title, score, description);
+                            mItemList.add(listViewItem);
+                            break;
+                        case "Good, Bad, Strange":
+                            listViewItem = new ListViewItem(nom3, title, score, description);
+                            mItemList.add(listViewItem);
+                            break;
+                        case "Titanic":
+                            listViewItem = new ListViewItem(titanic, title, score, description);
+                            mItemList.add(listViewItem);
+                            break;
+                        case "The Wolf of Wallstreet":
+                            listViewItem = new ListViewItem(wolf, title, score, description);
+                            mItemList.add(listViewItem);
+                            break;
+                    }
+                    mListView.setAdapter(mAdapter);
+                    mAdapter.notifyDataSetChanged();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     class ListViewAdapter extends BaseAdapter implements Serializable {
